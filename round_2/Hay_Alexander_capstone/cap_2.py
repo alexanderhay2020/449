fed6aa1caefc2e3ba9e30887736e1b1331b012a7
alex hay <alexanderhay2020@u.northwestern.edu>
2020-11-11 01:51:30 -0600

    end effector runs full trajectory path

--- round_2/Hay_Alexander_capstone/cap_2.py
+++ round_2/Hay_Alexander_capstone/cap_2.py
@@ -23,16 +23,29 @@ def shuffle(T, gripper_state):
                       T[0][3],T[1][3],T[2][3], gripper_state]
         
     return temp
-    # ref_traj = np.append(ref_traj,temp,)
 
 def close_gripper():
     # Gripper State: 1 = closed
     # Gripper takes 0.625s to open/close
+    
     t = 0.63
     N = t*k/0.01
 
     temp = ref_traj[-1]
     temp[-1] = 1
+    temp = np.full((63,13),temp)
+    for i in range(len(temp)):
+        ref_traj.append(temp[i])
+
+def open_gripper():
+    # Gripper State: 0 = closed
+    # Gripper takes 0.625s to open/close
+    
+    t = 0.63
+    N = t*k/0.01
+
+    temp = ref_traj[-1]
+    temp[-1] = 0
     temp = np.full((63,13),temp)
     for i in range(len(temp)):
         ref_traj.append(temp[i])
@@ -53,47 +66,67 @@ def TrajectoryGenerator(Tse_init, Tsc_init, Tsc_final, Tce_grasp, Tce_stand, k):
     N = t*k/0.01
     
     # Move end-effector to cube
-    Tse_si = np.dot(Tsc_init, Tce_stand)
-    traj = mr.CartesianTrajectory(Tse_init, Tse_si, t, N, 5)
+    Tse_is = np.dot(Tsc_init, Tce_stand)
+    traj = mr.CartesianTrajectory(Tse_init, Tse_is, t, N, 5)
     for i in range(len(traj)):
         temp = shuffle(traj[i], 0)
         ref_traj.append(temp)
     
     # Drop end-effector to cube height
-    Tse_gi = np.dot(Tsc_init, Tce_grasp) 
-    traj = mr.CartesianTrajectory(Tse_si, Tse_gi, t, N, 5)
+    Tse_ig = np.dot(Tsc_init, Tce_grasp) 
+    traj = mr.CartesianTrajectory(Tse_is, Tse_ig, t, N, 5)
     for i in range(len(traj)):
         temp = shuffle(traj[i], 0)
         ref_traj.append(temp)
 
     # Close gripper
     close_gripper()
-    # traj = ref_traj[-1]
-    # traj[-1] = 1
-    # traj = mr.CartesianTrajectory(traj, traj, 0.625, N, 5)
-    # for i in range(len(traj)):
-    #     temp = shuffle(traj[i], 0)
-    #     ref_traj.append(temp)
     
+    # Raise end-effector back up
+    traj = mr.CartesianTrajectory(Tse_ig, Tse_is, t, N, 5)
+    for i in range(len(traj)):
+        temp = shuffle(traj[i], 1)
+        ref_traj.append(temp)
+
+    # Move to final position
+    t = 4
+    N = t*k/0.01
+    Tse_fs = np.dot(Tsc_final, Tce_stand)         
+    traj = mr.CartesianTrajectory(Tse_is, Tse_fs, t, N, 5)
+    for i in range(len(traj)):
+        temp = shuffle(traj[i], 1)
+        ref_traj.append(temp)
+        
+    # Drop end-effector to final cube height
+    t = 2
+    N = t*k/0.01
+    Tse_fg = np.dot(Tsc_final, Tce_grasp) 
+    traj = mr.CartesianTrajectory(Tse_fs, Tse_fg, t, N, 5)
+    for i in range(len(traj)):
+        temp = shuffle(traj[i], 1)
+        ref_traj.append(temp)
+    
+    # Open gripper
+    open_gripper()
     
     print("Trajectory Generated")
 
     
 def main():
     
-    cube_init = np.transpose([[1, 0, 0]])               # (1m, 0m, 0 rad)
-    cube_final = np.transpose([[0, -1, -np.pi/2]])      # (0m,-1m, ) pi/2 rad)
+    # cube_init = np.transpose([[1, 0, 0]])               # (1m, 0m, 0 rad)
+    # cube_final = np.transpose([[0, -1, -np.pi/2]])      # (0m,-1m, ) pi/2 rad)
     
-    M = np.array([[ 1, 0, 0, 0.033 ],
-                  [ 0, 1, 0, 0     ],
-                  [ 0, 0, 1, 0.6546],
-                  [ 0, 0, 0, 1     ]])
+    # M = np.array([[ 1, 0, 0, 0.033 ],
+    #               [ 0, 1, 0, 0     ],
+    #               [ 0, 0, 1, 0.6546],
+    #               [ 0, 0, 0, 1     ]])
     
-    Blist = np.array([[ 0, 0, 1,  0,      0.033, 0],
-                      [ 0,-1, 0, -0.5076, 0,     0],
-                      [ 0,-1, 0, -0.3526, 0,     0],
-                      [ 0,-1, 0, -0.2176, 0,     0],
-                      [ 0, 0, 1,  0,      0,     0]]).T
+    # Blist = np.array([[ 0, 0, 1,  0,      0.033, 0],
+    #                   [ 0,-1, 0, -0.5076, 0,     0],
+    #                   [ 0,-1, 0, -0.3526, 0,     0],
+    #                   [ 0,-1, 0, -0.2176, 0,     0],
+    #                   [ 0, 0, 1,  0,      0,     0]]).T
     
     Tse_init = np.array([[ 0, 0, 1, 0],
                          [ 0, 1, 0 ,0],
@@ -110,9 +143,15 @@ def main():
                           [ 0, 0, 1, 0.025],
                           [ 0, 0, 0, 1]])
     
+    Tce_grasp = np.array([[-0.5,       0, 0.8660254, 0.015],
+                          [ 0,         1, 0,         0],
+                          [-0.8660254, 0,-0.5,       0.02],
+                          [ 0,         0, 0,         1]])
     
-    Tce_grasp = np.array([[-0.5, 0, 0.8660254, 0.015],[0, 1, 0, 0],[-0.8660254,0, -0.5,0.02],[0, 0, 0, 1]])
-    Tce_stand = np.array([[0, 0, 1, -0.3],[0, 1, 0, 0],[-1, 0, 0, 0.5],[0, 0, 0, 1]])
+    Tce_stand = np.array([[ 0, 0, 1,-0.3],
+                          [ 0, 1, 0, 0],
+                          [-1, 0, 0, 0.5],
+                          [ 0, 0, 0, 1]])
     	
     TrajectoryGenerator(Tse_init, Tsc_init, Tsc_final, Tce_grasp, Tce_stand, k)
     
